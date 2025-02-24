0bf09db0a558fdf75edd298571c46268ff4131e7
faisal-shk <faisalplayz007@gmail.com>
2025-02-24 14:30:14 +0530

    code updated

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+#  Create a Flask app instance
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+#  Move db initialization outside app_context()
+
+with app.app_context():
+    db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+# Add message
+@app.route("/", methods=['GET', 'POST'])
+def add_msg():
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo= Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+
+    allTodo = Todo.query.all()
+    return render_template("index.html", allTodo=allTodo)
+
+@app.route("/show")
+def show(): #  Show all todo
+    allTodo = Todo.query.all()
+    print(allTodo)
+    
+@app.route("/update/<int:sno>", methods=['GET', 'POST'])
+def update(sno): #  Update the todo
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")    
+    
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template("update.html", todo=todo)
+    
+@app.route("/delete/<int:sno>")
+def delete(sno): #  Delete the todo
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
+#  Create database inside application context
+with app.app_context():
+    db.create_all()
+    print("Database successfully!")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
+    print("Server is running on port 8000")
